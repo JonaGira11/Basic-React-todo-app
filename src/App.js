b597425a14c028107d294671b45b3c9e358f9d25
Jonthesallz <gitboi56@outlook.com>
2022-05-23 21:20:16 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Todolist from './TodoList';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Todolist/>
+    )
+  }
+}
+
+export default App;
